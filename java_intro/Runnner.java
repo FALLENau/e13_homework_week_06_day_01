5b73b68d893b82ebe4e9dbf3650aea436de75f22
FALLENau <reece.c.jones@gmail.com>
2017-06-26 09:57:15 +0100

    java start ponit

---
+++ java_intro/Runnner.java
@@ -0,0 +1,6 @@
+class Runner {
+  public static void main(String [] args) {
+    Cat mycat = new Cat();
+    mycat = new Dog();
+  }
+}//this main function is the startpoint for the java file and won't start without it.
