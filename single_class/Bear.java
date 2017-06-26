2e2274067be3fa8b002ec704a6455c868ec0bbda
FALLENau <reece.c.jones@gmail.com>
2017-06-26 13:49:16 +0100

    BearTest update

--- single_class/Bear.java
+++ single_class/Bear.java
@@ -1,8 +1,12 @@
 class Bear {
-  String name;
+  private String name;
+  private int age;
+  private double weight;
 
-  public Bear(String name) {
+  public Bear(String name, int age, double weight) {
     this.name = name;
+    this.age = age;
+    this.weight = weight;
   }
 
   public String getName() {
@@ -12,4 +16,13 @@ class Bear {
   public void setName(String newName) {
     this.name = newName;
   }
+  //Runner end
+  public int getAge() {
+    return this.age;
+  }
+
+  public double getWeight() {
+    return this.weight;
+  }
+
 }
--- single_class/BearTest.java
+++ single_class/BearTest.java
@@ -1,10 +1,30 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Before;
 
 public class BearTest {
+  Bear bear;
+
+  @Before
+  public void before() {
+    bear = new Bear("Baloo", 25, 95.62);
+  }
+
   @Test
   public void hasName() {
-    Bear bear = new Bear("Baloo");
+    // Bear bear = new Bear("Baloo", 25); //Note:these bears exist under class title and within @before or @beforeEach
     assertEquals("Baloo", bear.getName());
+  }
+
+  @Test
+  public void hasAge() {
+    // Bear bear = new Bear("Baloo", 25);
+    assertEquals(25, bear.getAge());
+  }
+
+  @Test
+  public void hasWeight() {
+    // Bear bear = new Bear("Baloo", 25, 95.62);
+    assertEquals(95.62, bear.getWeight(), 0.01);
   }
 }
